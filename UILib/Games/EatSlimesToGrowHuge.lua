84b5d7fbaef9cb4d6e60f642d86622af5150049a
EvolWareOfc <zenwarehub@gmail.com>
2026-09-16 11:04:48 +0100

    Update EatSlimesToGrowHuge.lua

--- UILib/Games/EatSlimesToGrowHuge.lua
+++ UILib/Games/EatSlimesToGrowHuge.lua
@@ -21,10 +21,10 @@ end)
 -- Black Hole Gamepass
 -- =====================
 L_3_:CreateButton('Unlock Black Hole Gamepass', function()
-	local L_14_ = game:GetService('ReplicatedStorage').Honeypot.Internal.RemoteStorage
+	local L_16_ = game:GetService('ReplicatedStorage').Honeypot.Internal.RemoteStorage
 			:FindFirstChild('BlackHoleGamepass - RemoteEvent')
-	if L_14_ then
-		L_14_:FireServer()
+	if L_16_ then
+		L_16_:FireServer()
 	end
 end)
 
@@ -32,10 +32,10 @@ end)
 -- Magnet Gamepass
 -- =====================
 L_3_:CreateButton('Unlock Magnet Gamepass', function()
-	local L_15_ = game:GetService('ReplicatedStorage').Honeypot.Internal.RemoteStorage
+	local L_17_ = game:GetService('ReplicatedStorage').Honeypot.Internal.RemoteStorage
 			:FindFirstChild('MagnetGamepass - RemoteEvent')
-	if L_15_ then
-		L_15_:FireServer()
+	if L_17_ then
+		L_17_:FireServer()
 	end
 end)
 
@@ -43,10 +43,10 @@ end)
 -- RequestTeleport Button
 -- =====================
 L_3_:CreateButton('Teleport', function()
-	local L_16_ = game:GetService('ReplicatedStorage').Honeypot.Internal.RemoteStorage
+	local L_18_ = game:GetService('ReplicatedStorage').Honeypot.Internal.RemoteStorage
 			:FindFirstChild('RequestTeleport - RemoteEvent')
-	if L_16_ then
-		L_16_:FireServer()
+	if L_18_ then
+		L_18_:FireServer()
 	end
 end)
 
@@ -54,10 +54,10 @@ end)
 -- LogGift Button
 -- =====================
 L_3_:CreateButton('LogGift Once', function()
-	local L_17_ = game:GetService('ReplicatedStorage').Honeypot.Internal.RemoteStorage
+	local L_19_ = game:GetService('ReplicatedStorage').Honeypot.Internal.RemoteStorage
 			:FindFirstChild('LogGift - RemoteEvent')
-	if L_17_ then
-		L_17_:FireServer({
+	if L_19_ then
+		L_19_:FireServer({
 			['TimerFinished'] = true,
 			['Type'] = 'Leaving',
 			['Maid'] = {
@@ -106,14 +106,14 @@ L_3_:CreateButton('LogGift Once', function()
 end)
 
 -- Utility function to claim a boost or reward
-local function L_8_func(L_18_arg0, L_19_arg1)
-	local L_20_ = game:GetService('ReplicatedStorage')
+local function L_8_func(L_20_arg0, L_21_arg1)
+	local L_22_ = game:GetService('ReplicatedStorage')
 		:WaitForChild('Honeypot')
 		:WaitForChild('Internal')
 		:WaitForChild('RemoteStorage')
 		:FindFirstChild('ClaimBoost - RemoteEvent')
-	if L_20_ then
-		L_20_:FireServer(L_18_arg0, L_19_arg1)
+	if L_22_ then
+		L_22_:FireServer(L_20_arg0, L_21_arg1)
 	else
 		warn('ClaimBoost - RemoteEvent not found!')
 	end
@@ -123,8 +123,8 @@ end
 -- Rainbow Boost Toggle
 -- =====================
 local L_9_ = false
-L_4_:CreateToggle('Rainbow Boost', function(L_21_arg0)
-	L_9_ = L_21_arg0
+L_4_:CreateToggle('Rainbow Boost', function(L_23_arg0)
+	L_9_ = L_23_arg0
 	if L_9_ then
 		task.spawn(function()
 			while L_9_ do
@@ -139,8 +139,8 @@ end)
 -- BlobSize Boost Toggle
 -- =====================
 local L_10_ = false
-L_4_:CreateToggle('BlobSize Boost', function(L_22_arg0)
-	L_10_ = L_22_arg0
+L_4_:CreateToggle('BlobSize Boost', function(L_24_arg0)
+	L_10_ = L_24_arg0
 	if L_10_ then
 		task.spawn(function()
 			while L_10_ do
@@ -155,13 +155,13 @@ end)
 -- Claim Gift Toggle
 -- =====================
 local L_11_ = false
-L_4_:CreateToggle('Claim Gift (+10k Size)', function(L_23_arg0)
-	L_11_ = L_23_arg0
+L_4_:CreateToggle('Claim Gift (+10k Size)', function(L_25_arg0)
+	L_11_ = L_25_arg0
 	if L_11_ then
 		task.spawn(function()
 			while L_11_ do
 				-- Fire LogGift
-				local L_24_ = {
+				local L_26_ = {
 					[1] = {
 						['TimerMilestones'] = {
 							1,
@@ -209,30 +209,34 @@ L_4_:CreateToggle('Claim Gift (+10k Size)', function(L_23_arg0)
 					},
 				}
 				game:GetService('ReplicatedStorage').Honeypot.Internal.RemoteStorage['LogGift - RemoteEvent']
-					:FireServer(unpack(L_24_))
+					:FireServer(unpack(L_26_))
 
 				-- Fire ClaimGift
-				local L_25_ = {
+				local L_27_ = {
 					'db8a0918-eded-4ae2-917f-0bba92e657b8'
 				}
 				game:GetService('ReplicatedStorage').Honeypot.Internal.RemoteStorage['ClaimGift - RemoteEvent']
-					:FireServer(unpack(L_25_))
+					:FireServer(unpack(L_27_))
 				task.wait(0.1)
 			end
 		end)
 	end
 end)
 local L_12_ = loadstring(
-    game:HttpGet('https://raw.githubusercontent.com/DozeIsOkLol/LibWare/refs/heads/main/games/Tabs/SupportedGames.lua'))()
-
--- Execute the Supported Games function, passing the main window and library
-L_12_(Window, L_1_)
+    game:HttpGet('https://raw.githubusercontent.com/EvolWareOfc/OldHubsMadeByEvol/refs/heads/main/UILib/Games/UniversalTab.lua'))()
+L_12_(L_2_, L_1_)
 local L_13_ = loadstring(
-    game:HttpGet('https://raw.githubusercontent.com/DozeIsOkLol/LibWare/refs/heads/main/games/Tabs/Misc.lua'))()
+    game:HttpGet('https://raw.githubusercontent.com/EvolWareOfc/OldHubsMadeByEvol/refs/heads/main/UILib/Games/SupportedGames.lua'))()
+L_13_(L_2_, L_1_)
+local L_14_ = loadstring(
+    game:HttpGet('https://raw.githubusercontent.com/EvolWareOfc/OldHubsMadeByEvol/refs/heads/main/UILib/Games/Misc.lua'))()
+L_14_(L_2_, L_1_)
+local L_15_ = loadstring(
+    game:HttpGet('https://raw.githubusercontent.com/EvolWareOfc/OldHubsMadeByEvol/refs/heads/main/UILib/Games/Credits.lua'))()
+L_15_(L_2_, L_1_)
 
--- Execute the Misc tab function
-L_13_(Window, L_1_)
+-- Misc systems
 loadstring(
-    game:HttpGet('https://raw.githubusercontent.com/DozeIsOkLol/LibWare/refs/heads/main/games/Misc/Developer%20Detection%20System.lua'))()
+    game:HttpGet('https://raw.githubusercontent.com/EvolWareOfc/OldHubsMadeByEvol/refs/heads/main/UILib/Games/Developer%20Detection%20System.lua'))()
 loadstring(
-    game:HttpGet('https://raw.githubusercontent.com/DozeIsOkLol/LibWare/refs/heads/main/games/Misc/DevTag.lua'))()
+    game:HttpGet('https://raw.githubusercontent.com/EvolWareOfc/OldHubsMadeByEvol/refs/heads/main/UILib/Games/DevTag.lua'))()
